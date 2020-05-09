c2c766c69765a453907d124a180e1e54807a5f20
Stefan Tzeggai <stefan.tzeggai@value-ag.de>
2020-05-09 08:42:21 +0200

    Remove empty test class

--- Python/tests/test_csv.py
+++
@@ -1,8 +0,0 @@
-# Python script to query REST-API from empirica-systeme, see https://www.empirica-systeme.de/en/portfolio/empirica-systeme-rest-api/
-# This work is licensed under a "Creative Commons Attribution 4.0 International License", sett http://creativecommons.org/licenses/by/4.0/
-# Documentation of REST-API at https://api.empirica-systeme.de/api-docs/
-
-def test_init():
-    assert 1==1
-
-
